641feb1dcc53c3e738bfe96b59c675fccef86b03
ayappasigatapu143 <ayappa.sigatapu143gmail.com>
2020-03-31 14:42:16 +0530

    my first git project

---
+++ server/src/main/java/com/info/beans/model/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.info.beans.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.info.beans.config.EmployeeConfig;
+
+@SpringBootApplication
+@Import(EmployeeConfig.class)
+public class ServerApplication {
+  
+ 	 public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
