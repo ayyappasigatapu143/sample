cd4e82b7015db67d46e567d21d832b7f0ae70d9f
ayappasigatapu143 <ayappa.sigatapu143gmail.com>
2020-03-31 17:34:09 +0530

    second

--- server/src/main/java/com/info/beans/controller/EmployeeController.java
+++ server/src/main/java/com/info/beans/controller/EmployeeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.info.beans.model.Authentication;
 import com.info.beans.model.Employee;
 import com.info.beans.service.EmployeeService;
 
