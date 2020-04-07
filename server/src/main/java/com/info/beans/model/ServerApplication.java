709703ddc675ad9fbf935169b0be98277c04ee2f
ayappasigatapu143 <ayappa.sigatapu143gmail.com>
2020-04-07 09:27:03 +0530

    assfasf

--- server/src/main/java/com/info/beans/model/ServerApplication.java
+++ server/src/main/java/com/info/beans/model/ServerApplication.java
@@ -13,7 +13,7 @@ public class ServerApplication {
  	 public static void main(String[] args) {
 
          String x=System.getProperty("env.name");
-        System.out.println("sacjasnvjnsajv++++++++++++++++++++++++++++++++++++++++++++"+x);
+        System.out.println("sacjasnvjnsajv++++++++++++++++++++++++++++++++++++++++++++"+x+args[0]);
 
                 
 		SpringApplication.run(ServerApplication.class, args);
